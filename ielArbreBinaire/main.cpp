b0592ef09f6874bec867e7ab079bfad9e8efd532
noker\Noureddine <nkerzazi@gmail.com>
2019-10-17 05:52:23 -0400

    mineure

--- ielArbreBinaire/main.cpp
+++ ielArbreBinaire/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include<cstdlib>
+#include "Arbre.h"
+
+using namespace std;
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int monTableau[16]={51, 77, 22, 5, 16, 33, 65, 53, 15, 101, 84, 3, 4, 71, 88, 81};
+
+    ARBRE arbre;
+    for (int i=0; i<16; i++){
+        arbre.AjouterNoeud(monTableau[i]);
+    }
+
+    arbre.AfficherArbre();
     return 0;
 }
